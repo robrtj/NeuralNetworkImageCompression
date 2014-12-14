e0554841838e65d62628f8eb00b2a987339d6941
Paweł Bielicki <bielicki91@gmail.com>
2014-12-14 16:52:23 +0100

    add structure of mutation

--- src/main/java/pl/edu/pw/mini/nn/neat/Genotype.java
+++ src/main/java/pl/edu/pw/mini/nn/neat/Genotype.java
@@ -4,6 +4,7 @@ import org.encog.util.identity.GenerateID;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Pawel on 2014-12-14.
@@ -18,7 +19,39 @@ public class Genotype {
     }
 
     public Genotype(GenerateID generateID){
+        super();
         innovationNumber = generateID;
+    }
+
+    public void mutation(){
+        Random random = new Random();
+        MutationType mutationType = MutationType.getMutationType(random.nextDouble());
+
+        switch (mutationType){
+            case AddConnection:
+                AddConnection();
+                break;
+            case AddNode:
+                AddNode();
+                break;
+            case DeleteConnection:
+                DeleteConnection();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void AddNode() {
+        
+    }
+
+    private void DeleteConnection() {
+
+    }
+
+    private void AddConnection() {
+
     }
 
     public FFCompressNetwork createFastForwardNetwork(){
