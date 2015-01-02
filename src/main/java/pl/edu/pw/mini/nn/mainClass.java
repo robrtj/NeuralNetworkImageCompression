fa1a4147d6bd57e103772df2944b6f6db51588a7
Robert Jakubowski <jeszczemoge@gmail.com>
2015-01-03 00:15:04 +0100

    add main class and example of configuration

---
+++ src/main/java/pl/edu/pw/mini/nn/mainClass.java
@@ -0,0 +1,12 @@
+package pl.edu.pw.mini.nn;
+
+/**
+ * Created by Robert on 2015-01-02.
+ */
+public class MainClass {
+    public static void main(final String args[]) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
