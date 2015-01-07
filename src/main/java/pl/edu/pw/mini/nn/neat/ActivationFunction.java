b42c2752aa85e4b7d33505872ea0e059c8fdadd4
Paweł Bielicki <bielicki91@gmail.com>
2015-01-08 00:06:27 +0100

    add activationUniPolar function

--- src/main/java/pl/edu/pw/mini/nn/neat/ActivationFunction.java
+++ src/main/java/pl/edu/pw/mini/nn/neat/ActivationFunction.java
@@ -4,9 +4,7 @@ package pl.edu.pw.mini.nn.neat;
  * Created by Pawel on 2015-01-06.
  */
 public interface ActivationFunction {
-    //get value of f(x)
-    double getValue(double x);
-
     //return activation value
-    double compute(double[] input);
+    double compute(double input);
 }
+
